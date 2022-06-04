edd87078e74e8986cc44d6c8e54b73f392e580ca
Aiden <7mu777@gmail.com>
2022-06-04 21:03:46 +0800

    initial commit

---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,24 @@
+const { deployments, network, ethers, getNamedAccounts } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert, expect } = require("chai")
+const { inputToConfig } = require("@ethereum-waffle/compiler")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async () => {
+          let fundMe
+          let deployer
+          const sendValue = ethers.utils.parseEther("1")
+          beforeEach(async () => {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+          it("allow people to fund and withdraw", async () => {
+              await fundMe.fund({ value: sendValue })
+              await fundMe.withdraw()
+              const endingBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              assert.equal(endingBalance.toString(), "0")
+          })
+      })
